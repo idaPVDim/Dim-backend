12786929fd90a99481d3a9d3bac2823b7380fe43
codebadolo <codewithbadolo@gmail.com>
2026-01-05 12:25:30 +0100

    changes

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,7 +3,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth import get_user_model
 
-from .models import Conversation, Message, MessageNotification
+from .models import Conversation, Message
 
 User = get_user_model()
 
---
+++ idav/settings_test.py
@@ -0,0 +1,5 @@
+from .settings import *
+
+DEBUG = True
+
+SECURE_SSL_REDIRECT = False
