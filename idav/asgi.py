4f5eefc310a334510fd75623a93879247b6ed699
codebadolo <codewithbadolo@gmail.com>
2026-01-07 04:18:42 +0100

    asgi.py

--- idav/asgi.py
+++ idav/asgi.py
@@ -1,16 +1,25 @@
-"""
-ASGI config for idav project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'idav.settings')
+django.setup()
 
-application = get_asgi_application()
+# Application Django standard pour HTTP
+django_application = get_asgi_application()
+
+# Pour les WebSockets (si vous en avez besoin)
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        # Ici vous pouvez ajouter la gestion des WebSockets
+        # Pour l'instant, on rejette les connexions WebSocket
+        await send({
+            'type': 'websocket.close',
+            'code': 1000
+        })
+    else:
+        raise NotImplementedError(f"Unknown scope type: {scope['type']}")
