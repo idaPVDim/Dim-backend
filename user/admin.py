500ffbc71b4b39690e1dd8413c2b74ca89ced065
codebadolo <codewithbadolo@gmail.com>
2026-01-21 17:26:30 +0100

    admin.py

---
+++ user/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import User
+# Register your models here.
+admin.site.register(User)  # Replace ... with your model names to register them in the admin site.
