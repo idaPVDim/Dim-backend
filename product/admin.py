c0ef5aac40c61cbd0536b6836c12b13543831ea4
codebadolo <codewithbadolo@gmail.com>
2026-01-27 18:34:57 +0100

    admin

--- product/admin.py
+++ product/admin.py
@@ -1,21 +1,6 @@
 from django.contrib import admin
-from .models import Product, Category, Brand
+from .models import Equipement ,Marque, Categorie
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'brand', 'price', 'stock', 'is_active')
-    list_filter = ('category', 'brand', 'is_active')
-    search_fields = ('name', 'description')
-    ordering = ('name',)
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    search_fields = ('name',)
-    ordering = ('name',)
-@admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin): 
-    list_display = ('name', 'country')
-    search_fields = ('name',)
-    ordering = ('name',)
-    
+admin.site.register(Equipement)
+admin.site.register(Marque)
+admin.site.register(Categorie)
